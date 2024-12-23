3a51d04c669a622fd3d2b943acbabd440374fd33
jban28 <jd.bannister28@gmail.com>
2024-12-23 16:14:14 +0000

    swap useLocation for useParams to get post ID from url

--- src/pages/BlogPost/BlogPost.jsx
+++ src/pages/BlogPost/BlogPost.jsx
@@ -1,6 +1,6 @@
 import { generateClient, post } from "aws-amplify/data";
 import { useState, useEffect } from "react";
-import { useLocation } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import Markdown from "react-markdown";
 import Navigation from "../../components/Navigation/Navigation.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
@@ -10,30 +10,33 @@ import "./BlogPost.css";
 const client = generateClient();
 
 const getPost = async function (postId) {
-  return await client.models.blogPost.get( { id: postId } );
-}
+  return await client.models.blogPost.get({ id: postId });
+};
 
 const BlogPost = () => {
   const [postData, setPostData] = useState({});
-  
-  const location = useLocation();
+
+  const routeParams = useParams();
 
   useEffect(() => {
-    const postId = location.pathname.split('/').pop()
-    
-    getPost(postId)
-    .then((data) => {
-      console.log(data)
-      setPostData(data)
-    })
-  }, [location])
+    const postId = routeParams.postId;
+    console.log(postId);
+    getPost(postId).then((data) => {
+      console.log(data);
+      setPostData(data);
+    });
+  }, [location]);
 
   return (
     <>
       <Navigation />
       <article className="blog-post__post">
         <h1 className="blog-post__title">{postData.title}</h1>
-        <PostData date={postData.date} category={postData.category} arrTags={postData.tags} />
+        <PostData
+          date={postData.date}
+          category={postData.category}
+          arrTags={postData.tags}
+        />
         <Markdown className="blog-post__content">{postData.content}</Markdown>
       </article>
       <Footer />
