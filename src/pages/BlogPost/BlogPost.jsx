5365cb7f0f2a86960d42d94663e1f31b794b973e
jban28 <jd.bannister28@gmail.com>
2024-12-24 11:42:12 +0000

    add syntax highlighter for code blocks in markdown

--- src/pages/BlogPost/BlogPost.jsx
+++ src/pages/BlogPost/BlogPost.jsx
@@ -2,19 +2,36 @@ import { generateClient, post } from "aws-amplify/data";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
 import Navigation from "../../components/Navigation/Navigation.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import PostData from "../../components/PostData/PostData.jsx";
 import "./BlogPost.css";
+import "highlight.js/styles/github.css";
 
 const client = generateClient();
 
 const getPost = async function (postId) {
-  const {data: postData, errors} = await client.models.blogPost.get({ id: postId });
-  if (!postData) {
-    throw "No data retrieved"
+  // Test data
+  if (true) {
+    return {
+      id: "testid",
+      title: "Post title",
+      lastRevised: "2024-12-24",
+      category: "a category",
+      tags: ["tag1", "tag2"],
+      content:
+        "# hello \n some text \n ```js \n const something = function () { return 'hello'; } \n```",
+    };
   }
-  return postData
+
+  const { data: postData, errors } = await client.models.blogPost.get({
+    id: postId,
+  });
+  if (!postData) {
+    throw "No data retrieved";
+  }
+  return postData;
 };
 
 const BlogPost = () => {
@@ -26,31 +43,37 @@ const BlogPost = () => {
   useEffect(() => {
     const postId = routeParams.postId;
     getPost(postId)
-    .then((postData) => {
-      setPostData(postData);
-      setNoPost(false);
-    })
-    .catch((err) => {
-      setNoPost(true);
-    });
+      .then((postData) => {
+        setPostData(postData);
+        setNoPost(false);
+      })
+      .catch((err) => {
+        setNoPost(true);
+      });
   }, [location]);
 
   return (
     <>
       <Navigation />
-      {!noPost ? 
-      <article className="blog-post__post">
-        <h1 className="blog-post__title">{postData.title}</h1>
-        <PostData
-          date={postData.lastRevised}
-          category={postData.category}
-          arrTags={postData.tags}
-        />
-        <Markdown className="blog-post__content">{postData.content}</Markdown>
-      </article>
-      : <div className="blog-post__error">Post not found</div>}
+      {!noPost ? (
+        <article className="blog-post__post">
+          <h1 className="blog-post__title">{postData.title}</h1>
+          <PostData
+            date={postData.lastRevised}
+            category={postData.category}
+            arrTags={postData.tags}
+          />
+          <Markdown
+            rehypePlugins={[rehypeHighlight]}
+            className="blog-post__content"
+          >
+            {postData.content}
+          </Markdown>
+        </article>
+      ) : (
+        <div className="blog-post__error">Post not found</div>
+      )}
       <Footer />
-      
     </>
   );
 };
