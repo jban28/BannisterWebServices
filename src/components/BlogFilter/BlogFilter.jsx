1a34761be1c53084e3a26d9d67d05dccd97e5aac
jban28 <jd.bannister28@gmail.com>
2025-01-02 11:05:13 +0000

    add CategoryInput component to BlogFilter

--- src/components/BlogFilter/BlogFilter.jsx
+++ src/components/BlogFilter/BlogFilter.jsx
@@ -1,5 +1,25 @@
+import { useRef, useState } from "react";
+import "./BlogFilter.css"
+
+const CategoryInput = ({ name, desc, checked, setChecked }) => {
+  const inputRef = useRef(null)
+
+  return (
+      <label className={"category-input" + (checked ? " category-input__checked" : "")}>
+        <input className="category-input--checkbox" checked={checked} ref={inputRef} type="checkbox" onChange={(e) => { setChecked(e.target.checked) }}/>
+        <dfn className="category-input--name">{name}</dfn>
+        :&nbsp;
+        {desc}
+      </label>
+  )
+}
+
 const BlogFilter = () => {
-  return <div></div>;
+  const [checked, setChecked] = useState(false);
+
+  return <div>
+    <CategoryInput name="Test" desc="a description of the category" checked={checked} setChecked={setChecked} />
+  </div>;
 };
 
 export default BlogFilter;
