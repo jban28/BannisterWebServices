58c744a023ad37becc0a6d9cf120077a4724e938
jban28 <jd.bannister28@gmail.com>
2025-01-03 14:00:36 +0000

    add RestoreScroll component to scroll back to top on navigation

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 // CSS import order matters - want to apply custom css and component css after bs css
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import React from "react";
+import { StrictMode, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Portfolio from "./pages/Portfolio/Portfolio.jsx";
 import NoPage from "./pages/NoPage/NoPage.jsx";
@@ -16,9 +16,20 @@ import HomeLayout from "./layouts/HomeLayout/HomeLayout.jsx";
 
 Amplify.configure(outputs);
 
+const RestoreScroll = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return <></>;
+};
+
 export default function App() {
   return (
     <BrowserRouter>
+      <RestoreScroll />
       <Routes>
         <Route element={<HomeLayout />}>
           <Route index path="/" element={<Home />} />
@@ -36,7 +47,7 @@ export default function App() {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
