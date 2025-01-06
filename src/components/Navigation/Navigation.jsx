3a06af8979666830fa3986d862792603beac1129
jban28 <jd.bannister28@gmail.com>
2025-01-06 21:55:53 +0000

    remove bootstrap from navbar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,46 +1,33 @@
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 import { useLocation, Link } from "react-router-dom";
 import { forwardRef } from "react";
-import "./Navigation.css";
+import styles from "./Navigation.module.css";
+
+const NavLink = ({ children, path }) => {
+  const currentPath = useLocation().pathname;
+  const active = path === currentPath;
+  return (
+    <Link
+      className={styles.navLink + (active ? ` ${styles.active}` : "")}
+      to={`${path}`}
+    >
+      {children}
+    </Link>
+  );
+};
 
 const Navigation = forwardRef(function ({ opacity = 1 }, ref) {
-  const routerLocation = useLocation();
-
   return (
-    <Navbar
-      id="navbar"
-      sticky="top"
-      className="justify-content-center navigation"
+    <nav
+      className={styles.navBar}
       style={{ backgroundColor: `rgba(255, 42, 0, ${opacity})` }}
       ref={ref}
     >
-      <Nav justify variant="underline">
-        <Nav.Item>
-          <Nav.Link as={Link} active={routerLocation.pathname === "/"} to="/">
-            Home
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            as={Link}
-            active={routerLocation.pathname === "/portfolio"}
-            to="/portfolio"
-          >
-            Portfolio
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            as={Link}
-            to="/blog"
-            active={routerLocation.pathname === "/blog"}
-          >
-            Blog
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Navbar>
+      <div className={styles.navList}>
+        <NavLink path="/">Home</NavLink>
+        <NavLink path="/portfolio">Portfolio</NavLink>
+        <NavLink path="/blog">Blog</NavLink>
+      </div>
+    </nav>
   );
 });
 
