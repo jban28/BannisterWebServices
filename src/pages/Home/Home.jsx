faa05e22736bfb67f947d1d43d2d92850b4f4761
jban28 <jd.bannister28@gmail.com>
2025-01-09 15:13:34 +0000

    convert to css module

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,15 @@
 import Button from "../../components/Button/Button.jsx";
 import BrandIcon from "../../components/BrandIcon/BrandIcon.jsx";
 import ContactForm from "../../components/ContactForm/ContactForm.jsx";
-import "./Home.css";
+import styles from "./Home.module.css";
 import arrBrands from "../../config/brands.json";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <div className="homepage__container homepage__container--white">
-        <p className="homepage__intro homepage__max-width">
+      <div className={`${styles.container} ${styles.containerWhite}`}>
+        <p className={`${styles.intro} ${styles.pageWidth}`}>
           I am James Bannister, an aspiring developer. I established Bannister
           Web Services as a way for me to develop my programming skills and
           showcase my work. I provide free web and software development services
@@ -21,15 +21,15 @@ const Home = () => {
         </Button>
       </div>
 
-      <div className="homepage__container homepage__container--theme">
-        <div className="homepage__brand-bar">
+      <div className={`${styles.container} ${styles.containerTheme}`}>
+        <div className={styles.brandBar}>
           {arrBrands.map(({ id, label }) => (
             <BrandIcon key={id} id={id} label={label} />
           ))}
         </div>
       </div>
-      <div className="homepage__container">
-        <ContactForm className="homepage__max-width" />
+      <div className={styles.container}>
+        <ContactForm className={styles.pageWidth} />
       </div>
     </>
   );
