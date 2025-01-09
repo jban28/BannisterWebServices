a24c1676d913048b300110c0118c73b9f764ba2d
jban28 <jd.bannister28@gmail.com>
2025-01-09 09:31:33 +0000

    configure cypress for testing

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import pluginReact from "eslint-plugin-react";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { ignores: ["**/*.cy.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
