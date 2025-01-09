a209bb8a986e1585c02213fb14f6cdfc58bd3310
jban28 <jd.bannister28@gmail.com>
2025-01-09 11:08:59 +0000

    update to css module

--- src/components/BrandIcon/BrandIcon.jsx
+++ src/components/BrandIcon/BrandIcon.jsx
@@ -1,9 +1,9 @@
-import "./BrandIcon.css";
+import styles from "./BrandIcon.module.css";
 
 const BrandIcon = ({ id, label }) => {
   return (
-    <i className={`brand-icon__icon fa-brands fa-${id}`}>
-      <span className="brand-icon__tooltip">{label}</span>
+    <i className={`${styles.icon} fa-brands fa-${id}`}>
+      <span className={styles.tooltip}>{label}</span>
     </i>
   );
 };
